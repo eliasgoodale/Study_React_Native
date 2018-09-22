bcc2b7ba1dc30549f5d138b073d56b6a8f952163
Elias Goodale <egoodale@e1z4r6p8.42.us.org>
2018-09-21 17:43:30 -0700

    chapt 1 page 19 examples updated

--- Examples/propsExample.js
+++ Examples/propsExample.js
@@ -5,9 +5,15 @@ export default class Title extends Component {
 
     render() {
         return (
-            //This is how props are inserted into a container
-            <h1>
-                { this.props.titleText }
+            //This is how props are inserted into an element
+            // Props may also be used to define attributes of an element
+            <h1
+                style = {{
+                    backgroundColor: this.props.highlighted ? 'yellow' : 'white',
+                    fontSize: `${this.props.fontSize}px`
+                }}
+                >
+            { this.props.titleText }
             </h1>
         );
     }
@@ -16,8 +22,27 @@ export default class Title extends Component {
 // *this* is how we define props belonging to Title 
 Title.propTypes = {
     // You may use the .isRequired to indicate required type of a prop
-    titleText: PropTypes.string.isRequired
+    titleText: PropTypes.string.isRequired,
     // You may use other types as well
-    highlighted: PropTypes.bool
-    fontSize: PropTypes.number
+    highlighted: PropTypes.bool,
+    fontSize: PropTypes.number,
+    //You may also use the .oneOfType function as follows
+    fontSizeVariableType: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+    // You may specify a set of values that a property is allowed to take by using the oneOf method
+    size: PropTypes.oneOf([
+        'small',
+        'medium',
+        'large'
+    ]),
+    // We may also specify more complex data types such as arrays and complex objects
+    simpleArray: PropTypes.array,
+    simpleObject: PropTypes.object,
+    arrayOfNumbers: PropTypes.arrayOf(PropTypes.number),
+    complexObject: PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string
+    }),
 };
