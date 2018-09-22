e7f1e0215d06cd9a5c1a6c0430fe371e9c199093
eliasgoodale <eligoodale1@gmail.com>
2018-09-22 13:46:10 -0700

    Starting the day, first commit working on event handlers

--- Examples/React/eventHandlers.js
+++ Examples/React/eventHandlers.js
@@ -2,13 +2,17 @@ import React, { Component, PropTypes } from 'react';
 import Title from './Title'
 
 export default class NewsItem extends Component {
-
+	constructor(props) {
+		super(props);
+		// We do this here because React internally optimizes to make it more efficient
+		this.onClick = this.onClick.bind(this);
+	}
 	render() {
 		return (
 			<div 
 				className="news-item"
-				//We are binding the render method's this context to the onClick method when adding it as a click handler
-				onClick={this.onClick.bind(this)}
+				// We are binding the render method's this context to the onClick method when adding it as a click handler, however, there is a better way to do this by adding a this binding within the components constructor
+				// onClick={this.onClick.bind(this)}
 			>
       	<Image />
       	<Title />
