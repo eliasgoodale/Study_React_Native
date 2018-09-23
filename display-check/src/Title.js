ca93b4b46c8afa6b6b4196ba5939408820ee02b6
eliasgoodale <eligoodale1@gmail.com>
2018-09-22 17:01:02 -0700

    finished section on internal state

---
+++ display-check/src/Title.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+export default class Title extends Component {
+
+	render() {
+		return (
+			<h1
+				
+				style ={{
+					backgroundColor: this.props.highlighted ? 'yellow' : 'white',
+					fontSize: `${this.props.fontSize}px`
+				}}
+			>
+				{this.props.titleText}
+			</h1>
+		);
+	}
+}
+
+ Title.propTypes = {
+ 	titleText: PropTypes.string.isRequired,
+ 	hilighted: PropTypes.bool,
+ 	fontSize: PropTypes.number,
+ }
+
+ Title.defaultProps = {
+ 	highlighted: false,
+ 	fontSize: 18,
+ }
